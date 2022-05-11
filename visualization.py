09e562006a18ce67073f7813ab64dde61ac67558
Andrew Chen <1062446749@qq.com>
2022-05-11 21:20:53 +0800

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -3,14 +3,17 @@ from PIL import Image
 import numpy as np
 
 
-def visualization(x, mask):
+def visualization(x, mask, color = 255, weight = 0):
     '''
     visualization for semantic segmentation
     :param x: pytorch tensor [1, 3, m, n]    0<=x<=1 so you need to * 255 to visualize it
     :param mask: pytorch tensor [1,1, m, n],   values are 0 or 1, is the mask.
     :return: a PIL image which can directly image.show
     '''
-    x = (x + mask) / 2
+    mask_tensor = mask * color / 255
+    non_masked = (1-mask) * x     #没mask的地方有数值
+    masked = mask * x*weight + mask_tensor* mask*(1-weight)               # mask的地方有数值
+    x = non_masked+masked
     x = x.permute(0, 2, 3, 1)
     x = x.squeeze().numpy()
     x *= 255
